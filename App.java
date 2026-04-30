247741097afd72735a3d7d6fc0c1072ec00a6c7b
bessonovmaksim86-beep <bessonovmaksim86@gmail.com>
2026-05-01 01:07:04 +0300

    Add files via upload

--- App.java
+++ App.java
@@ -1,8 +1,11 @@
-package com.example;
-import org.apache.commons.lang3.StringUtils;
-
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class App {
     public static void main(String[] args) {
-        System.out.println(StringUtils.reverse("Hello, World!"));
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        System.out.println(org.apache.commons.lang3.StringUtils.reverse("Hello, World!"));
     }
 }
