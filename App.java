e31126747a9b0a6ca09158c6cf626d337bfcab67
bessonovmaksim86-beep <bessonovmaksim86@gmail.com>
2026-05-01 00:50:49 +0300

    Create App.java

---
+++ App.java
@@ -0,0 +1,8 @@
+package com.example;
+import org.apache.commons.lang3.StringUtils;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println(StringUtils.reverse("Hello, World!"));
+    }
+}
